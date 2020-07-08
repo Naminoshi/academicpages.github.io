75b7b01791a89708ebba79171edc2a7deff3969f
Michel Aguena <michel.aguena@gmail.com>
2020-07-08 20:10:09 -0300

    update publications

--- markdown_generator/publications_bib.py
+++ markdown_generator/publications_bib.py
@@ -15,11 +15,19 @@ def journals(key):
         return key
 
 mon_num = {v.lower(): "%02d"%k for k, v in enumerate(calendar.month_abbr) if k>0}
-
+def rm_spaces(string):
+    base = string
+    test = base.replace('\t', ' ')
+    test = test.replace('  ', ' ')
+    while test!=base:
+        base = test
+        test = base.replace('  ', ' ')
+    return base
 def get_info(data_line):
     data_use = data_line.split(' = ')
     keys = [d.split(',')[-1] for d in data_use][:-1]
-    values = [d.split(',')[0] for d in data_use]
+    values = [','.join(d.split(',')[:-1]) for d in data_use]
+    values = [rm_spaces(v) for v in values]
     out = {k: '' for k in ('author', 'title', 'journal', 'volume', 'page', 'month', 'year')}
     out['ref'] = values[0].replace('.', '')
     for k, v in zip(keys, values[1:]):
@@ -29,6 +37,20 @@ def get_info(data_line):
     if 'howpublished' in out and out['journal']=='':
         out['journal'] = out['howpublished']
     return out
+def abb_author(authors):
+    if len(authors.split(' and '))>2:
+        return '%s et al'%authors.split(' and ')[0].split(',')[0]
+    else:
+        return authors
+def vol_page(pd):
+    if pd['volume']==pd['page']=='':
+        return ''
+    elif pd['volume']=='':
+        return ' %s,'%pd['page']
+    elif pd['page']=='':
+        return ' %s,'%pd['volume']
+    else:
+        return ' %s:%s,'%(pd['volume'], pd['page'])
 def gen_file(pd):
     print(pd)
     date = '%s-%s-01'%(pd['year'], mon_num[pd['month'].lower()])
@@ -43,8 +65,8 @@ def gen_file(pd):
         'venue: "%s"'%journals(pd['journal']),
         'paperurl: "%s"'%pd['adsurl'],
         #'citation: "Your Name, You. (2009). &quot;Paper Title Number 1.&quot; <i>Journal 1</i>. 1(1)."',
-        'citation: "%s. &quot;%s.&quot; <i>%s</i>, %s:%s, %s %s"'%(pd['author'], pd['title'], journals(pd['journal']),
-                         pd['volume'], pd['page'], pd['month'].capitalize(), pd['year']),
+        'citation: "%s. &quot;%s.&quot; <i>%s</i>,%s %s %s"'%(pd['author'], pd['title'], journals(pd['journal']),
+                         vol_page(pd), pd['month'].capitalize(), pd['year']),
         '---',
         '',
         '[*ArXiv*](https://arxiv.org/abs/%s), [*ADS*](%s)'%(pd['eprint'], pd['adsurl']),
@@ -52,8 +74,9 @@ def gen_file(pd):
         #'[Download paper here](http://academicpages.github.io/files/paper1.pdf)',
         #'',
         #'Recommended citation: Your Name, You. (2009). "Paper Title Number 1." <i>Journal 1</i>. 1(1).',
-        'Recommended citation: "%s. &quot;%s.&quot; <i>%s</i>, %s:%s, %s %s"'%(pd['author'], pd['title'], journals(pd['journal']),
-                         pd['volume'], pd['page'], pd['month'].capitalize(), pd['year']),
+        'Recommended citation: "%s (%s). &quot;%s.&quot; <i>%s</i>,%s %s %s"'%(abb_author(pd['author']),
+                         pd['year'], pd['title'], journals(pd['journal']),
+                         vol_page(pd), pd['month'].capitalize(), pd['year']),
         ])
     print '\n****%s\n_____\n%s\n'%(filename, content)
     return filename, content
